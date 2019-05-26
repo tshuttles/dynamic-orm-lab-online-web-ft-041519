02e49f6e3f64d73ed5538bb26058c83a83cfbea9
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-26 23:17:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,6 +12,22 @@ class InteractiveRecord
   def initialize 
   end 
   
+  def table_name_for_insert 
+  end 
   
+  def col_names_for_insert 
+  end 
+  
+  def values_for_insert 
+  end 
+  
+  def save 
+  end 
+  
+  def self.find_by_name 
+  end
+  
+  def self.find_by 
+  end 
   
 end

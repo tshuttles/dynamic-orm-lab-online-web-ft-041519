345dce12fa5bb640d113bc2d64001bba5567b820
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-26 23:31:18 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,7 +11,12 @@ class InteractiveRecord
     DB[:conn].results_as_hash = true 
     
     sql = "pragma table_info('#{table_name}')"
-    
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
   end 
   
   def initialize 

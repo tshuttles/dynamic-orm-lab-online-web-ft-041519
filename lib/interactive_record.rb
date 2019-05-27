05edf78e1468c97e4603fc95288c4a06fc53115d
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-27 00:11:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,7 +19,7 @@ class InteractiveRecord
     column_names.compact
   end 
   
-  def initialize 
+  def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end

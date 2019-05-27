c54fb5c8f3fabc9118adcebb64127cd2bc6c71f0
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-27 00:27:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -45,7 +45,7 @@ class InteractiveRecord
     sql = "INSERT INTO #{table_name_for_insert} 
       (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql) 
-    
+    @id = DB[:conn].execute(sql)
   end 
   
   def self.find_by_name 
